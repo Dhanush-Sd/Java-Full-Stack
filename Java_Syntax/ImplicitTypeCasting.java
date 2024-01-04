241c06319e116ea05b03c69d72f7753fb9db7df2
Dhanush S <dhanushseenivasan0403@gmail.com>
2024-01-04 21:31:19 +0530

    Implicit typecasting

---
+++ Java_Syntax/ImplicitTypeCasting.java
@@ -0,0 +1,8 @@
+package Java_Syntax;
+
+public class ImplicitTypeCasting {
+    public static void main(String[] args) {
+        byte a = 20;//implicit typecasting(int is converted into byte)by compiler
+        System.out.println(a);
+    }
+}
