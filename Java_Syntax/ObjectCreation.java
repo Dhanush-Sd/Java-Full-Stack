456510a4392dcfc296ff62c294dcf21283eab3f6
Dhanush S <dhanushseenivasan0403@gmail.com>
2024-01-04 21:30:51 +0530

    Object Creation

---
+++ Java_Syntax/ObjectCreation.java
@@ -0,0 +1,11 @@
+package Java_Syntax;
+public class ObjectCreation{
+    int a = 10;
+
+    public static void main(String[] args) {
+        ObjectCreation o = new ObjectCreation();//object creation
+        System.out.println(o.a);//accessing variable using object
+    }
+}
+
+
