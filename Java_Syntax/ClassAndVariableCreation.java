255b3ca6ef3b5aafe6a5268f4726e281fc190ae3
Dhanush S <dhanushseenivasan0403@gmail.com>
2024-01-04 21:27:39 +0530

    Class and Variable creation

---
+++ Java_Syntax/ClassAndVariableCreation.java
@@ -0,0 +1,9 @@
+package Java_Syntax;
+public class ClassAndVariableCreation { //class creation
+    static int a = 20;
+    public static void main(String[] args) {
+        int b = 22;//variable creation
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
