db939eec0f48946938b395fa51eda3c262685152
Dhanush S <dhanushseenivasan0403@gmail.com>
2024-01-05 14:49:08 +0530

    Math Class

---
+++ Oops/MathClass.java
@@ -0,0 +1,18 @@
+package Oops;
+import java.lang.Math;
+public class MathClass {
+    public static void main(String[] args) {
+        double a = Math.toRadians(90);
+        double b = Math.sin(a);
+        System.out.println(b);
+        System.out.println(Math.min(10,20));
+        System.out.println(Math.max(10,20));
+        System.out.println(Math.abs(-4.5));
+        System.out.println(Math.ceil(4.4));
+        System.out.println(Math.floor(4.4));
+        System.out.println(Math.cbrt(5));
+        System.out.println(Math.sqrt(5));
+        System.out.println(Math.pow(3,2));
+
+    }
+}
