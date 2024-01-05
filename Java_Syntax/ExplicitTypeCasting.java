753a492a9a4e3ade35ce8d59e309bb708723c23e
Dhanush S <dhanushseenivasan0403@gmail.com>
2024-01-05 11:24:57 +0530

    Explicit typecasting

---
+++ Java_Syntax/ExplicitTypeCasting.java
@@ -0,0 +1,8 @@
+package Java_Syntax;
+
+public class ExplicitTypeCasting {
+    public static void main(String[] args) {
+        byte a = (byte)128;//byte ranges from -128 to 127,here integer 128 is converted into byte by user
+        System.out.println(a);
+    }
+}
