c46627aeebeeda38c9115d0a60452bc125598a1a
Dhanush S <dhanushseenivasan0403@gmail.com>
2024-01-09 22:22:02 +0530

    Another Way Of Integer Class Declaration

---
+++ WrapperClass/AnotherWayOfIntegerClassDeclaration.java
@@ -0,0 +1,11 @@
+package WrapperClass;
+
+public class AnotherWayOfIntegerClassDeclaration {
+    public static void main(String[] args) {
+        Integer a = 10;
+        Integer b = 20;
+        int c = a + b;
+        System.out.println(c);
+
+    }
+}
