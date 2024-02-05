ec3160444901d6484c53614ed1973e474f0c094c
Dhanush S <dhanushseenivasan0403@gmail.com>
2024-02-05 12:02:00 +0530

    TreeMap example

---
+++ CollectionFramework/TreeMapExample.java
@@ -0,0 +1,14 @@
+package CollectionFramework;
+
+import java.util.TreeMap;
+
+public class TreeMapExample {
+    public static void main(String[] args) {
+        TreeMap<String,Integer> t = new TreeMap<String,Integer>();
+        t.put("Dhanush",1);
+        t.put("Abinash",2);
+        t.put("Bala",3);
+        t.put("Aakash",4);
+        System.out.println(t);
+    }
+}
