50e3d6f8c8da5f95b8b89e247442dc3060b1d327
Dhanush S <dhanushseenivasan0403@gmail.com>
2024-02-05 12:01:09 +0530

    Advanced For Loop Example

---
+++ CollectionFramework/AdvancedForLoopExample.java
@@ -0,0 +1,10 @@
+package CollectionFramework;
+
+public class AdvancedForLoopExample {
+    public static void main(String[] args) {
+        int A[]= new int[]{6,9,2,8,1,4,12};
+        for(int x:A){
+            System.out.println(x);
+        }
+    }
+}
