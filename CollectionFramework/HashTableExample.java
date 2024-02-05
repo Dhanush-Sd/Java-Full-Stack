161f86e8d60b23bfaabc97ada343701890ec5bc8
Dhanush S <dhanushseenivasan0403@gmail.com>
2024-02-05 12:02:15 +0530

    HashTable example

---
+++ CollectionFramework/HashTableExample.java
@@ -0,0 +1,14 @@
+package CollectionFramework;
+
+import java.util.Hashtable;
+
+public class HashTableExample {
+    public static void main(String[] args) {
+        Hashtable h = new Hashtable();
+        h.put("Dhanush",1);
+        h.put("Abinash",2);
+        h.put("Bala",3);
+        h.put("Aakash",4);
+        System.out.println(h);
+    }
+}
